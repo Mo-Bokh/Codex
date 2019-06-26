43df385859cac2bb39fe0d7f6122651e837859aa
bokhammh <bokhammh@kw-16071.kaust.edu.sa>
2019-06-26 15:27:54 +0300

    Tried to do a for loop to collect Identity matrices from each from to plot a collective progress. The matrix is flipped and didnt work. ( identity matrix is too thin.

--- TestGround.m
+++ TestGround.m
@@ -1,34 +1,51 @@
 
+clear all; close all; clc
+
 SPEstruct = load_SPE_filetype
 
 A = SPEstruct.data{1,1} ;
 B = SPEstruct.data{1,2};
-%F = zeros(1024);
+
+
+% 
+% for i=5:1:10
+% P = SPEstruct.data{1,i} .*(SPEstruct.data{1,i}>630);
+% figure;imagesc(P')
+% end
+
+
+I = (eye(41));
+I = I'; I = I == 1;
+M = zeros (1024);
+
+
+
+for i = 1:1:119 ; 
+C= SPEstruct.data{1,i} .*(SPEstruct.data{1,i}>600);
+S= C(478+i*4:518+i*4 , 113:153);
 F=zeros(1024);
+X = S.*I;
+F(478+i*4:518+i*4 , 113:153) = X;
+M = M + F; 
 
-for i=2:1:119
-    X = SPEstruct.data{1,i} ;
-    A = (A + X)/2;
-end
+end 
 
-for i=1:1:119
-    D = SPEstruct.data{1,i} ;
-    F = F + D;
-end
 
-C = F.*(F>620);
 
-figure(1) = imagesc(B','CDataMapping','scaled')
+%G = SPEstruct.data{1,1}.*(SPEstruct.data{1,1}>630)
+%figure;imagesc() 
+%C = A.*(A>640);
+%C = C';
+%figure; imagesc(C,'CDataMapping','scaled')
 
 %C = A.*(A>630);
 
-
-
-
-
-
-
-
+% 
+% S = C(478:518 , 113:153);
+% 
+% 
+% X = S.*I
+% 
 
 
 
