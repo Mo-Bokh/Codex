0307fbaf6c3301e724709ddf9bb19b96a8c8c618
bokhammh <bokhammh@kw-16071.kaust.edu.sa>
2019-06-27 11:23:43 +0300

    fixed the slope of the mask and made it take five data points for each row. Here is the result

--- TestGround.m
+++ TestGround.m
@@ -4,6 +4,7 @@ clear all; close all; clc
 SPEstruct = load_SPE_filetype
 
 A = SPEstruct.data{1,1} ;
+A = A.*(A>630);
 B = SPEstruct.data{1,2};
 
 
@@ -13,22 +14,50 @@ B = SPEstruct.data{1,2};
 % figure;imagesc(P')
 % end
 
+Z = zeros (177,29);
+ 
+ 
+ for i=1:1:177
+     
+    for j=1:1:177
+       
+        if (i==6*j) 
+        
+            for k=0:1:5
+        
+                for l=0:1:4
+                    
+                    Z(i + k,j + l) = 1;
+                
+                end
+        
+            end
+        
+        end
+        
+    end
+     
+ end
 
-I = (eye(41));
-I = I'; I = I == 1;
-I = fliplr(I);
-M = zeros (1024);
+Z = flipud(Z);
+
+
+% I = (eye(41));
+% I = I'; I = I == 1;
+% I = fliplr(I);
+ M = zeros (1024);
 
 
 
 for i = 1:1:119 ; 
 C= SPEstruct.data{1,i} .*(SPEstruct.data{1,i}>600);
 C = C';
-S= C(478:518 , 113+i*4:153+i*4);
+S= C(460:638 , 117+i*4:149+i*4);
 F=zeros(1024);
-X = S.*I;
-F(478:518 , 113+i*4:153+i*4) = X;
+X = S.*Z;
+F(460:638 , 117+i*5:149+i*5) = X;
 M = M + F; 
+
 end 
 
 figure; imagesc(M)
