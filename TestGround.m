f6372b823fc10b1624d225ce0613cdd38703ff95
bokhammh <bokhammh@kw-16071.kaust.edu.sa>
2019-07-01 16:29:38 +0300

    created another mask to test inner side of flame. will work once I get the averaging algorithim from Gshgari

--- TestGround.m
+++ TestGround.m
@@ -3,7 +3,7 @@ clear all; close all; clc
 
 SPEstruct = load_SPE_filetype
 
-A = SPEstruct.data{1,119} ;
+A = SPEstruct.data{1,1} ;
 A = A.*(A>630);
 B = SPEstruct.data{1,2};
 
@@ -41,12 +41,8 @@ Z = zeros (177,29);
 
 Z = flipud(Z);
 
-
-% I = (eye(41));
-% I = I'; I = I == 1;
-% I = fliplr(I);
- M = zeros (1024);
-
+M = zeros (1024);
+M2 = zeros (1024);
 
 
 for i = 1:1:119 ; 
@@ -55,7 +51,7 @@ for i = 1:1:119 ;
 C= SPEstruct.data{1,i} .*(SPEstruct.data{1,i}>630); %Extract data of each frame
 C = C'; 
 
-figure;imagesc(C)
+
 
 SG = sgolayfilt(sum(C),3,51); % smooth the summation of data files to find place of concentration
 % figure; plot(SG)
@@ -63,26 +59,31 @@ SG = sgolayfilt(sum(C),3,51); % smooth the summation of data files to find place
 
 disp(locs(1))
 
-S= C(460:638 , (locs(1)-16):(locs(1)+16)); %cut matrix from data
+SD= C(460:638 , (locs(1)-16):(locs(1)+16)); %cut matrix from data
+SD2 = C(460:638 , (locs(1)-3):(locs(1)+29));
+
+
 
 F=zeros(1024); %initialize empty matrix to apply mask on
+F2=zeros(1024);
 
-X = S.*Z; % apply mask on data
 
+X = SD.*Z; % apply mask on data
+X2 = SD2.*Z
 
 
 F(460:638 , (locs(1)-16):(locs(1)+16)) = X; % insert masked data in the empty matrix
-
+F2(460:638 , (locs(1)-3):(locs(1)+29)) = X2;
 %figure;imagesc(F)
 
 
 M = M + F;  % add masked data together
-
+M2 = M2 + F2;
 
 end 
 
-%figure; imagesc(M)
-% figure; imagesc(A')
+
+ figure; imagesc(A')
 % figure; plot(sum(A'))
 % SG = sgolayfilt(sum(A'),3,31);
 
@@ -111,4 +112,5 @@ end
 
 %figure(2) = imagesc(C','CDataMapping','scaled')
 figure;imagesc(M)
+figure;imagesc(M2)
 colorbar
